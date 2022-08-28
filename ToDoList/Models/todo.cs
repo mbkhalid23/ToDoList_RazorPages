e80969c09bfb7da77535e8d1efd94e0e15b33ff2
mbkhalid23 <mbkhalid23@gmail.com>
2022-08-28 11:28:16 +0500

    Create todo model.

---
+++ ToDoList/Models/todo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class todo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Task { get; set; }
+        [Display(Name ="Due Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy", ApplyFormatInEditMode = true)]
+        public DateTime? DueDate { get; set; }
+        public string? priority { get; set; }
+    }
+}
